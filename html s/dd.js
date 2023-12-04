be3cec0d1f1d252d631c9edcd0eb77f488568a64
ayoola89 <ayoola.obafemi46@gmail.com>
2023-12-04 14:08:56 +0100

    schoolsite

---
+++ html s/dd.js
@@ -0,0 +1,53 @@
+// document.body.style.backgroundColor ='blue'
+const allstudentsinmyclass=[
+    "jude",
+    "Mr Ayoola",
+    "Jonathan",
+    "jerry",
+    {
+        name: "vinde",
+        tutor: "martin",
+        courses: ['css', 'html']
+
+    },
+    {
+        name: 'mike',
+        tutor: 'martin',
+        courses: ['js','vibe']
+    }
+]
+
+
+
+// console.log(allstudentsinmyclass[4].tutor)
+// console.log(allstudentsinmyclass[5].courses[1])
+
+// const obj1 ={
+//     firstname: 'peter',
+//     lastname: 'mike',
+//     age: 49,
+//     gender: 'female',
+//     hobbies: ['eating','sleeping'],
+//     likes: ['reading','sleeping'],
+//     address: {
+//         street: "78, bolude str, 67b avenue",
+//         city: "Ibadan",
+//         names: ["Avenue1", "Avenue2"]
+//     }
+// }
+// console.log(obj1.address.street)
+// console.log(obj1.address.names)
+
+// function 
+// params
+function Ayoola(a,b, c) {
+    console.log(a*b+c)
+}
+Ayoola(20,5,4)
+
+function ay (a,b){
+    console.log("firstname","lastname")
+}
+ay(ayool,babatunde)
+
+
